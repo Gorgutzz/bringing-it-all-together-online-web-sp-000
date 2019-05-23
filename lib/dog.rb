d421a74144b3660f4f02541e0d004ee4927ad807
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-23 22:58:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog
 
-  attr_accessor :name, :breed, :id=nil
+  attr_accessor :name, :breed
+  attr_reader :id
 
-  def initialize(name:, breed:, id:)
+  def initialize(id: nil, name:, breed:)
+    @id = id
     @name = name
     @breed = breed
-    @id = id
   end
 
 
